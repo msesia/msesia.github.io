0ca9ab974caedb56dc0e7fb5a2ebe47c83e0f0c3
msesia <msesia@users.noreply.github.com>
2026-06-10 04:31:20 +0000

    deploy: 7dec222d69d9999175c4125326141e1fd9371055

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Research Group",
-        "excerpt":"🎓 Current Ph.D. Students Tianmin Xie (5th year) Department: Data Sciences and Operations Research Focus: Conformal Prediction Yunjie Fan (4-th year) Department: Mathematics Research Focus: Conformal Prediction 🎓 Past Ph.D. Students Yanfei Zhou (5th year) Department: Data Sciences and Operations Graduated: 2026 Placement: Research Scientist, Meta Ziyi Liang Department: Mathematics...","categories": [],
+        "excerpt":"🎓 Current Ph.D. Students Tianmin Xie (5th year) Department: Data Sciences and Operations Research Focus: Conformal Prediction Yunjie Fan (4-th year) Department: Mathematics Research Focus: Conformal Prediction 🎓 Past Ph.D. Students Yanfei Zhou Department: Data Sciences and Operations Graduated: 2026 Placement: Research Scientist, Meta Ziyi Liang Department: Mathematics Graduated: 2024...","categories": [],
         "tags": [],
         "url": "/group/",
         "teaser": null
