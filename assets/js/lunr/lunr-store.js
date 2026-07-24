f87d08e70fb3527ae26a890edd641567b716c073
msesia <msesia@users.noreply.github.com>
2026-07-24 16:12:59 +0000

    deploy: 05943908152962dd828b3029dce12e5f4a671130

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -12,7 +12,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Publications",
-        "excerpt":"2026 Collective Outlier Detection and Enumeration with Conformalized Closed Testing 🎯 C. Magnani, M. Sesia, A. Solari Journal of the American Statistical Association (recently accepted), 2026 [Preprint] [BibTeX] Audited Conformal Prediction for Classification under Unknown Distribution Shift 🎯 Y. Zhou, R. Fathony, N. Nguyen, M. Sesia arXiv preprint, 2026 [Preprint]...","categories": [],
+        "excerpt":"2026 Conformal Prediction for Regression with Clipped Outcomes 🎯 M. Sesia, V. Svetnik arXiv preprint, 2026 [Preprint] [BibTeX] Collective Outlier Detection and Enumeration with Conformalized Closed Testing 🎯 C. Magnani, M. Sesia, A. Solari Journal of the American Statistical Association (recently accepted), 2026 [Preprint] [Published] [BibTeX] Audited Conformal Prediction for...","categories": [],
         "tags": [],
         "url": "/publications/",
         "teaser": null
@@ -303,6 +303,12 @@ var store = [{
         "excerpt":" ","categories": [],
         "tags": [],
         "url": "/publications/2026-4-collective-outlier-detection/",
+        "teaser": null
+      },{
+        "title": "Conformal Prediction for Regression with Clipped Outcomes",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/publications/2026-5-clipped/",
         "teaser": null
       },{
         "title": "Introduction to R (Stats 195)",
